1623572e9ec382f814c86db02e6e9984a45cc170
Vadim Maslov <vg.maslov@gmail.com>
2022-07-25 17:44:07 +0100

    Implementation of test view for /list endpoint testing.

---
+++ ForretasAPITester/Controllers/ListProductsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using ForretasAPITester.API;
+using ForretasAPITester.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForretasAPITester.Controllers
+{
+    public class ListProductsController : Controller
+    {
+        private readonly ForretasAPI api;
+
+        public ListProductsController(ForretasAPI forretasAPI)
+        {
+            api = forretasAPI;
+        }
+
+        public IActionResult Index()
+        {
+            ListProductsData data = new ListProductsData();
+            return View(data);
+        }
+
+        public IActionResult IndexWithResponse(ListProductsData data)
+        {
+            return View("Index", data);
+        }
+
+        [HttpPost]
+        public IActionResult SendListRequest([FromForm] ListProductsData data)
+        {
+            var apiLogin = User.Claims.FirstOrDefault(c => c.Type == "APILogin").Value.ToString();
+            var apiPassword = User.Claims.FirstOrDefault(c => c.Type == "APIPassword").Value.ToString();
+            int merchantId = Convert.ToInt32(User.Claims.FirstOrDefault(c => c.Type == "MerchantId").Value.ToString());
+
+            bool authenticated = api.Authenticate(apiLogin, apiPassword, out string jwt);
+
+            string endpoint = "/api/products/list";
+
+            string query = $"offset={data.Offset}&limit={data.Limit}";
+
+            string response = api.GenericGet(endpoint, query, jwt);
+
+            data.APIResponse = response;
+
+            return RedirectToAction("IndexWithResponse", data);
+        }
+    }
+}
---
+++ ForretasAPITester/Models/ListProductsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ForretasAPITester.Models
+{
+    public class ListProductsData
+    {
+        public int Offset { get; set; }
+        public int Limit { get; set; }
+
+        public string APIResponse { get; set; }
+
+        public ListProductsData()
+        {
+        }
+    }
+}
