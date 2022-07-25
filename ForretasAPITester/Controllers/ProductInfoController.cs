85ade2ba5f3efe4bf43781c9cf26166ad1b15476
Vadim Maslov <vg.maslov@gmail.com>
2022-07-25 18:05:14 +0100

    Implementation of testing interface for API /product endpoint

---
+++ ForretasAPITester/Controllers/ProductInfoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using ForretasAPITester.API;
+using ForretasAPITester.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForretasAPITester.Controllers
+{
+    public class ProductInfoController : Controller
+    {
+        private readonly ForretasAPI api;
+
+        public ProductInfoController(ForretasAPI forretasAPI)
+        {
+            api = forretasAPI;
+        }
+
+        public IActionResult Index()
+        {
+            ProductInfoData data = new ProductInfoData();
+            return View(data);
+        }
+
+        public IActionResult IndexWithResponse(ProductInfoData data)
+        {
+            return View("Index", data);
+        }
+
+        [HttpPost]
+        public IActionResult SendGetProductRequest([FromForm] ProductInfoData data)
+        {
+            var apiLogin = User.Claims.FirstOrDefault(c => c.Type == "APILogin").Value.ToString();
+            var apiPassword = User.Claims.FirstOrDefault(c => c.Type == "APIPassword").Value.ToString();
+            int merchantId = Convert.ToInt32(User.Claims.FirstOrDefault(c => c.Type == "MerchantId").Value.ToString());
+
+            bool authenticated = api.Authenticate(apiLogin, apiPassword, out string jwt);
+
+            string endpoint = "/api/products/product";
+
+            string query = $"id={data.ProductId}";
+
+            string response = api.GenericGet(endpoint, query, jwt);
+
+            data.APIResponse = response;
+
+            return RedirectToAction("IndexWithResponse", data);
+        }
+    }
+}
---
+++ ForretasAPITester/Models/ProductInfoData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForretasAPITester.Models
+{
+    public class ProductInfoData
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter valid product Id")]
+        public int ProductId { get; set; }
+
+        public string APIResponse { get; set; }
+
+        public ProductInfoData()
+        {
+        }
+    }
+}
